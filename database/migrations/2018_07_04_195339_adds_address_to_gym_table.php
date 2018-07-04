448b652f81ff5453fae5b5fc6721f3a6cd29eb58
Githinji Wanjohi <gyth.wanjohi@gmail.com>
2018-07-04 23:21:59 +0300

    Routes rectification

---
+++ database/migrations/2018_07_04_195339_adds_address_to_gym_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddsAddressToGymTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gyms', function (Blueprint $table) {
+            $table->string('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gyms', function (Blueprint $table) {
+            $table->dropColumn(['rating']);
+        });
+    }
+}
