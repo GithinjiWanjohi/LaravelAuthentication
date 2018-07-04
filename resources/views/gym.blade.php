315c533996e7483109a4e25f2159bca013b3b057
Githinji Wanjohi <gyth.wanjohi@gmail.com>
2018-07-04 22:55:07 +0300

    Routes rectification

--- resources/views/gym.blade.php
+++ resources/views/gym.blade.php
@@ -27,6 +27,20 @@
                             </div>
 
                             <div class="form-group row">
+                                <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="address" type="text" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="gym_name" value="{{ old('address') }}" required autofocus>
+
+                                    @if ($errors->has('address'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
                                 <label for="latitude" class="col-md-4 col-form-label text-md-right">{{ __('Latitiude') }}</label>
 
                                 <div class="col-md-6">
